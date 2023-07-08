d0a7a08dc96a5e93452280c6cc1401d882d209b7
Anton <https://a-shdv@github.com>
2023-07-08 11:31:10 +0400

    Functions panic(), recover() and defer

--- main.go
+++ main.go
@@ -3,32 +3,34 @@ package main
 import "fmt"
 
 func main() {
-	// 2D array
-	matrix := make([][]int, 10)
+	messages := [3]int{0, 1, 2}
 
-	// C-like loops
-	for i := 0; i < len(matrix); i++ {
-		for j := 0; j < len(matrix); j++ {
-			matrix[j] = make([]int, 10) // allocate memory for inner slice
-		}
+	defer handlePanic()
+
+	// START HERE
+	for i := range messages {
+		messages[i+1]++
 	}
 
-	// Golang style
-	for i := range matrix {
-		for j := range matrix {
-			matrix[i][j] = j
-		}
+	// DEFER (additional 50 ms)
+	defer printSmth()
+
+	fmt.Println("0")
+	fmt.Println("1")
+	fmt.Println("2")
+	defer fmt.Println("test")
+	fmt.Println("3")
+	fmt.Println("4")
+}
+
+// RARE
+func handlePanic() {
+	if r := recover(); r != nil {
+		fmt.Println(r)
+		fmt.Println("handlePanic() successfully executed")
 	}
+}
 
-	// While
-	//for true {
-	//	fmt.Println(matrix)
-	//}
-
-	fmt.Println(matrix)
-
-	// Key-value
-	for key, value := range matrix {
-		fmt.Println(key, value)
-	}
+func printSmth() {
+	fmt.Println("smth...")
 }
