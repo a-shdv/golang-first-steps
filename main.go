cb8491aff48f28c402cc4d74c1cbc9896c859449
Anton <https://a-shdv@github.com>
2023-07-20 10:36:39 +0400

    Interfaces info

--- main.go
+++ main.go
@@ -2,49 +2,17 @@ package main
 
 import (
 	"fmt"
-	"go-first-steps/structs"
-	"reflect"
+	"go-first-steps/shapes"
 )
 
 func main() {
-	// struct initialization
-	user := structs.User{
-		Name:   "Tony",
-		Age:    22,
-		Gender: "male",
-		Weight: 80.24,
-		Height: 181.44,
-	}
+	circle := shapes.Circle{Radius: 12.25}
+	square := shapes.Square{Length: 12.25}
+	printArea(circle)
+	printArea(square)
+}
 
-	// print struct
-	user.PrintUserInfo()
-	user.PrintUserInfoWithPointer("unknown")
-	user.PrintUserInfo()
-
-	fmt.Println()
-
-	fmt.Printf("%+v\n\n", user)
-
-	// Receiver by value and by reference
-	fmt.Printf("Name: %s\n", user.GetName())   // value
-	user.SetName("Tony")                       // reference
-	fmt.Printf("Name: %s\n\n", user.GetName()) // value
-
-	// Receiver by value and by reference
-	fmt.Printf("Name: %s\n", user.GetName()) // value
-	user.SetNameByValue("Unknown")           // value (makes a copy), hence, doesn't change the actual value
-	fmt.Printf("Name: %s\n", user.GetName()) // value
-
-	// type conversion
-	anotherUser := structs.User{
-		Name:   "Alien",
-		Age:    5,
-		Gender: "unknown",
-		Weight: 80.24,
-		Height: 181.44,
-	}
-	isLegal := anotherUser.Age.IsLegal()
-	fmt.Println(isLegal)
-	fmt.Println(anotherUser.Age)
-	fmt.Println(reflect.TypeOf(anotherUser.Age))
+// Принимает любой объект
+func printArea(s shapes.Shape) {
+	fmt.Printf("Площадь фигуры: %.2f\n", s.Area())
 }
